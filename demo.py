3947926cded6cdfbf36a3dbb16ce805d175d57dd
Afridi P <uig01799@contiwan.com>
2024-01-05 16:52:30 +0530

    updated demo.py

--- demo.py
+++ demo.py
@@ -1,80 +1,89 @@
 class Node:
     def __init__(self, value) -> None:
-        self.value = value 
+        self.value = value
         self.next = None
-    
-class HashTable:
-    def __init__(self) -> None:
-        self.size = 0
-        self.table = {}
-    
-    def _hash(self, value):
-        return hash(value) % 7
-    
-    def insert(self, value):
-        hashkey = self._hash(value)
-        new_node = Node(value)
-        if not self.table.get(hashkey):
-            self.table[hashkey] = new_node
-        else:
-            currentnode = self.table[hashkey]
-            while(currentnode):
-                if currentnode.value == value:
-                    print("value found!")
-                    return
-                currentnode = currentnode.next
-            new_node.next = self.table[hashkey]
-            self.table[hashkey] = new_node
-        self.size += 1
-    
-    def delete(self, value):
-        hashkey = self._hash(value)
-        if self.table.get(hashkey):
-            previousnode = 0
-            currentnode = self.table[hashkey]
-            while(currentnode):
-                if currentnode.value == value:
-                    if previousnode:
-                        previousnode.next = currentnode.next
-                    else:
-                        self.table[hashkey] = currentnode.next
-                    self.size -= 1
-                    return
-                previousnode = currentnode
-                currentnode = currentnode.next
-    
-    def search(self, value):
-        hashkey = self._hash(value)
-        if self.table.get(hashkey):
-            currentnode = self.table[hashkey]
-            while currentnode:
-                if currentnode.value == value:
-                    print("value found! : ", currentnode.value)
-                else:
-                    print("iterating.. : ", currentnode.value)
-                currentnode = currentnode.next
-    
-    def display(self):
-        for k,v in self.table.items():
-            print(k, end=" : ")
-            currentnode = v
-            while(currentnode):
-                print(currentnode.value, end=", ")
-                currentnode = currentnode.next
-            print()
 
-obj = HashTable()
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+        else:
+            currentnode = self.head
+            while currentnode.next:
+                currentnode = currentnode.next
+            currentnode.next = new_node
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+    
+    def lookup(self, value):
+        currentnode = self.head
+        while currentnode:
+            if currentnode.value == value:
+                print("value found : ", value)
+                return
+            currentnode = currentnode.next
+    
+    def insert(self, pos, value):
+        new_node = Node(value)
+        currentnode = self.head
+        prevnode = 0
+        for i in range(pos-1):
+            prevnode = currentnode
+            currentnode = currentnode.next
+        if prevnode:
+            prevnode.next = new_node
+            new_node.next = currentnode
+        else:
+            self.prepend(value)
+
+    def iterate(self):
+        currentnode = self.head
+        while currentnode:
+            print(currentnode.value, end=", ")
+            currentnode = currentnode.next
+
+    def delete(self, value):
+        currentnode = self.head
+        prevnode = 0
+        while currentnode:
+            if currentnode.value == value:
+                if prevnode:
+                    prevnode.next = currentnode.next
+                    return
+                elif not prevnode and self.head.next:
+                    self.head = self.head.next
+                else:
+                    self.head = None
+            prevnode = currentnode
+            currentnode = currentnode.next
+
+obj = LinkedList()
 n=1
 while(n!=0):
-    print("1.Insert 2.Delete 3.Search 4.Display 5.Exit")
+    print("1.prepend 2.append 3.insert 4.lookup 5.Delete 6.iterate 8.Exit")
     n = int(input("enter the option : "))
     if n==1:
-        obj.insert(int(input("enter the value : ")))
+        obj.prepend(int(input("enter the value : ")))
     elif n==2:
-        obj.delete(int(input("enter the value : ")))
+        obj.append(int(input("enter the value : ")))
     elif n==3:
-        obj.search(int(input("enter the value : ")))
+        obj.insert(int(input("enter the index : ")), int(input("enter the value : ")))
     elif n==4:
-        obj.display()
+        obj.lookup(int(input("enter the value : ")))
+    elif n==5:
+        obj.delete(int(input("enter the value : ")))
+    elif n==6:
+        obj.iterate()
     else:
         n=0
+
